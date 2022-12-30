22609234a539e8a80f86b5217814d71f7ea44479
Yaroslav Slepukhin <yaroslavslepukhin@gmail.com>
2022-12-30 21:59:48 +0300

    fix header

--- source/js/animation.js
+++ source/js/animation.js
@@ -1,9 +1,9 @@
-const header = document.querySelector(".header__nav");
+const header = document.querySelector(".header");
 
 window.addEventListener('scroll', () => {
   if (window.scrollY >= 1000) {
-    header.classList.add('header__nav_color');
+    header.classList.add('header_color');
   } else {
-    header.classList.remove('header__nav_color');
+    header.classList.remove('header_color');
   }
 });
